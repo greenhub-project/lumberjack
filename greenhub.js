0386403711103ff63c37481c333ee6fe7ce5a924
Hugo Matalonga <hmatalonga@gmail.com>
2017-06-01 00:45:20 +0100

    add command count feature

--- greenhub.js
+++ greenhub.js
@@ -3,31 +3,77 @@ const chalk = require('chalk')
 const logSymbols = require('log-symbols')
 const Conf = require('conf')
 
-
 const greenhub = {
-  statusUrl: 'http://hmatalonga.com/greenhub/status.json',
-
-  testUrl: 'http://192.168.1.117:8080',
-
-  config: new Conf(),
+  version: '1.0.0',
 
   description: chalk.bold.green('GreenHub Lumberjack') +
   ' - A command line app for interacting with the GreenHub REST API.',
 
+  config: new Conf(),
+
+  server: {
+    url: {
+      status: 'http://hmatalonga.com/greenhub/status.json',
+      test: 'http://192.168.1.105:8080',
+    },
+    errors: {
+      failed: 'Server request has failed',
+    }
+  },
 
   // Commands
 
-  login: function(token, options) {
+  count: function(model = '', options = null) {
+    params = {}
+
+    console.log(chalk.blue('Fetching data from server...'))
+
+    if (options.last) {
+      const validate = options.last.match(/[1-9]+[0-9]*(m|w|d|h)/g) !== null
+
+      if (validate) {
+        params.last = options.last
+      } else {
+        console.log(logSymbols.warning, 'Invalid parameter!', options.last)
+        return
+      }
+    } else if (options.range) {
+      params.range = options.range
+    }
+
+    axios.get(this.makeUrl('/count/' + model), {
+      params: this.makeParams(params)
+    })
+    .then(response => {
+      if (response.data.total != undefined && !options.json) {
+        console.log(response.data.total)
+      } else {
+        console.log(this.prettyPrint(response.data))
+      }
+    })
+    .catch(error => {
+      console.log(logSymbols.error, chalk.red(this.server.errors.failed))
+      console.log(error.response.data)
+    })
+  },
+
+  login: function(token = '', options = null) {
     if (! this.config.has('server')) {
       console.log(logSymbols.warning, 'Server URL is not defined')
       console.log('Please attempt to login again...')
+
       this.fetchServerUrl()
       return
     }
+
     if (this.config.has('token') && !options.reload) {
-      console.log('Already logged in as', chalk.bold(this.config.get('name')))
+      console.log(
+        'Already logged in as',
+        chalk.bold(this.config.get('user').name)
+      )
       return
     }
+
     this.saveApiToken(token)
   },
 
@@ -36,12 +82,13 @@ const greenhub = {
       console.log('Not logged in...')
     } else {
       this.config.delete('token')
-      this.config.delete('name')
+      this.config.delete('user')
+
       console.log('Logout done!')
     }
   },
 
-  remote: function(options) {
+  remote: function(options = null) {
     if (options.fetch) {
       this.fetchServerUrl()
     } else {
@@ -61,7 +108,7 @@ const greenhub = {
     }
   },
 
-  whoami: function(options) {
+  whoami: function(options = null) {
     if (! this.config.has('token')) {
       console.log('Not logged in...')
     } else {
@@ -69,12 +116,19 @@ const greenhub = {
         this.displayUserInfo(this.config.get('user'), options.json)
       } else {
         console.log(chalk.blue('Fetching info from the server...'))
-        axios.get(this.getApiUrl('/user', this.config.get('token')))
+
+        axios.get(this.makeUrl('/user'), {
+          params: this.makeParams(params)
+        })
         .then(response => {
           if (response.data != null) {
             this.config.set('user', response.data)
             this.displayUserInfo(response.data, options.json)
           }
+        })
+        .catch(error => {
+          console.log(logSymbols.error, chalk.red(this.server.errors.failed))
+          console.log(error.response.data)
         })
       }
     }
@@ -84,28 +138,33 @@ const greenhub = {
 
   fetchServerUrl: function() {
     console.log(chalk.blue('Fetching server URL...'))
-    axios.get(this.statusUrl)
+
+    axios.get(this.server.url.status)
     .then(response => {
       console.log('Server URL found!')
       console.log(chalk.bold(response.data.server))
+
       this.config.set('server', response.data.server)
     })
     .catch(error => {
-      console.log(
-        logSymbols.error, chalk.red('Status server is not responding'), error
-      )
+      console.log(logSymbols.error, chalk.red(this.server.errors.failed))
+      console.log(error.response.data)
     })
   },
 
   saveApiToken: function(token = '') {
     console.log(chalk.blue('Signing in to GreenHub API server...'))
-    axios.get(this.getApiUrl('/user', token))
+
+    axios.get(this.makeUrl('/user'), {
+      params: this.makeParams(params)
+    })
     .then(response => {
       if (response.data != null) {
         console.log(
           logSymbols.success,
           'You are now logged in as ' + chalk.bold(response.data.name)
         )
+
         this.config.set('token', token)
         this.config.set('user', response.data)
       }
@@ -119,8 +178,9 @@ const greenhub = {
 
   displayUserInfo: function(user, isJson = false) {
     const status = user.verified ? logSymbols.success : logSymbols.error
+
     if (isJson) {
-      console.log(JSON.stringify(user, null, '    '))
+      console.log(this.prettyPrint(user))
     } else {
       console.log(chalk.bold(user.name), status)
       console.log(user.email)
@@ -128,10 +188,20 @@ const greenhub = {
     }
   },
 
-  getApiUrl: function(method = '', token = '', version = 1) {
-    token = token === '' ? this.config.get('token') : token
-    return this.testUrl + '/api/v' + version + method + '?api_token=' + token
+  makeUrl: function(method = '', version = 1) {
+    return this.server.url.test + '/api/v' + version + method
   },
+
+  makeParams: function(params = {}, token = '') {
+    params.cli = true
+    params.api_token = token === '' ? this.config.get('token') : token
+
+    return params
+  },
+
+  prettyPrint: function(el, format = '    ') {
+    return JSON.stringify(el, null, format)
+  }
 }
 
 module.exports = greenhub
--- index.js
+++ index.js
@@ -6,8 +6,28 @@ const program = require('commander')
 const greenhub = require('./greenhub')
 
 program
-  .version('1.0.0')
+  .version(greenhub.version)
   .description(greenhub.description)
+
+program
+  .command('count <model>')
+  .alias('c')
+  .description('return total number of records from specified model <devices, samples>')
+  .option('-j, --json', 'output result in JSON')
+  .option('-L, --last <interval>', 'amount of time interval of last <m>onth, <w>eek,<d>ay or <h>our')
+  .option('-R, --range [from]..[to]', 'time range of query in `yyyy-mm-dd` date format, optional arguments')
+  .action((model, options) => greenhub.count(model, options))
+  .on('--help', function() {
+    console.log('  Examples:')
+    console.log()
+    console.log('    $ greenhub count devices                           # all records')
+    console.log('    $ greenhub count samples --last 12h                # records of the last 12 hours')
+    console.log('    $ greenhub count devices -L 5d --json              # records of the last 5 days in json')
+    console.log('    $ greenhub count samples -R 2017-05-01:2017-05-31  # records between 2017-05-01 and 2017-05-31')
+    console.log('    $ greenhub count samples --range 2017-03-15:       # records from 2017-03-15 to current date')
+    console.log('    $ greenhub count samples --range :2017-02-01       # records before 2017-02-01')
+    console.log()
+  })
 
 program
   .command('login <token>')
@@ -21,12 +41,20 @@ program
   .action(() => greenhub.logout())
 
 program
-  .command('lumberjack')
+  .command('lumberjack <model> [params...]')
+  .alias('j')
   .description('flexible query builder that exports to different formats')
   .option('-A, --ascii', 'display on the stdin in ascii')
   .option('-H, --html', 'export to html file')
-  .option('-m, --model [name]', 'select the model to query [samples, devices]')
-  .action(options => console.log(greenhub.config))
+  .option('-L, --last <val><interval>', 'value of time interval of last <m>onth, <w>eek,<d>ay or <h>our')
+  .option('-R, --range [from]..[to]', 'time range of query in `yyyy-mm-dd` date format, optional arguments')
+  .action((model, params, options) => console.log(params))
+  .on('--help', function() {
+    console.log('  Examples:')
+    console.log()
+    console.log('    $ greenhub lumberjack devices brand:google  # params have the format [name:value]')
+    console.log()
+  })
 
 program
   .command('remote')
